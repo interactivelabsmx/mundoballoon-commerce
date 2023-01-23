4e97f09d1671e33a6058396a17586e8ba4309026
Luis Gonzalez <luis.gonzalez@airbnb.com>
2023-01-23 13:58:17 -0800

    Update table styles and packages

--- components/User/Events/EventsCompactTable.tsx
+++ components/User/Events/EventsCompactTable.tsx
@@ -37,63 +37,53 @@ const EventsCompactTable = () => {
             <tr>
               <th
                 scope="col"
-                className="py-3.5 pl-4 pr-3 text-center text-sm font-semibold text-gray-900 sm:pl-6"
+                className="px-3 py-3 text-sm font-semibold text-gray-900"
               >
                 {t('Event_Name')}
               </th>
               <th
                 scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                className="px-3 py-3 text-sm font-semibold text-gray-900"
               >
                 {t('Date')}
               </th>
               <th
                 scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                className="px-3 py-3 text-sm font-semibold text-gray-900"
               >
                 {t('Details')}
               </th>
-              <th
-                scope="col"
-                className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-              ></th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 bg-white">
             {userEvents.map((event) => (
               <Fragment key={event.userEventId}>
                 <tr>
-                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                    <div className="flex items-center">
-                      <span className="font-medium text-gray-900">
-                        {event.name}
-                      </span>
-                    </div>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 font-medium">
+                    {event.name}
                   </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                  <td className="whitespace-nowrap px-3 py-4 text-sm">
                     <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
                       <DateDisplayBase date={event.date} />
                     </span>
                   </td>
-                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    <span className="isolate inline-flex rounded-md shadow-sm">
-                      <SecundaryButton
-                        onClick={openDetails}
-                        className="mr-4"
-                        aria-label={t('More_Details')}
-                      >
-                        <EllipsisHorizontalCircleIcon
-                          className="h-4 w-4"
-                          aria-hidden="true"
-                        />
-                      </SecundaryButton>
-                      <SecundaryButton
-                        onClick={openDelete}
-                        aria-label={t('Delete')}
-                      >
-                        <TrashIcon className="h-4 w-4" aria-hidden="true" />
-                      </SecundaryButton>
-                    </span>
+                  <td className="whitespace-nowrap px-3 py-4">
+                    <SecundaryButton
+                      onClick={openDetails}
+                      className="mr-4"
+                      aria-label={t('More_Details')}
+                    >
+                      <EllipsisHorizontalCircleIcon
+                        className="h-4 w-4"
+                        aria-hidden="true"
+                      />
+                    </SecundaryButton>
+                    <SecundaryButton
+                      onClick={openDelete}
+                      aria-label={t('Delete')}
+                    >
+                      <TrashIcon className="h-4 w-4" aria-hidden="true" />
+                    </SecundaryButton>
                   </td>
                 </tr>
                 <Modal open={DetailsOpen} setOpen={setDetailsOpen}>
